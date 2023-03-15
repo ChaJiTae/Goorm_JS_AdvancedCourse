b9bab5437aa0e8315a88877604d36d4a284eb098
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-15 13:43:43 +0000

    #callback 함수

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
-var var2 = document.getElementsByName("passwd");
-console.log(var2);
+//function a(){
+//	console.log("2초가 걸리는 함수");
+//}
+//function b(){
+//	console.log("a다음에 실행되어야하는 b함수");
+//}
+//setTimeout(a,2000);
+//b();
 
-var var3 = document.getElementsByTagName("input");
-console.log(var3);
-
-// id 값에 어떤 값을 입력 -> 제출 버튼 클릭 --> 브라우저에서 "당신이 입력한 id값은 <입력한 값>입니다."
-
-function getInputValue(name){
-    var inputVal = document.getElementsByTagName("input")[0].value;
-    alert(`당신이 입력한 값은 ${inputVal}입니다.`);
+function a(callback){
+	setTimeout(function(){
+		console.log("2초가 걸리는 a함수");
+		callback();
+	},2000);
 }
 
+function b(){
+	console.log("a다음에 실행되어야하는 b함수");
+}
 
+a(b);
