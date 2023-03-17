78e356bc7b363cd278b920b8b34c083cd4d142ca
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-17 10:43:05 +0000

    변수 상황별로 쓰자

--- test.js
+++ test.js
@@ -1,21 +1,3 @@
-//function a(){
-//	console.log("2초가 걸리는 함수");
-//}
-//function b(){
-//	console.log("a다음에 실행되어야하는 b함수");
-//}
-//setTimeout(a,2000);
-//b();
-
-function a(callback){
-	setTimeout(function(){
-		console.log("2초가 걸리는 a함수");
-		callback();
-	},2000);
-}
-
-function b(){
-	console.log("a다음에 실행되어야하는 b함수");
-}
-
-a(b);
+const obj = {'key' : 'value' };
+obj.key = 'value2'; //value값은 바꿀 수 있음
+console.log(obj);
