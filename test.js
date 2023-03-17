07ac4d598b0c08903c35975e2a53b6b5147f1348
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-17 11:28:16 +0000

    #문자열 리터럴

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const obj = {'key' : 'value' };
-obj.key = 'value2'; //value값은 바꿀 수 있음
-console.log(obj);
+const name = "minchul";
+console.log("안녕 내 이름은 " + name + "이야");
+console.log(`안녕 내 이름은 ${name}이야.`);
+
+console.log("총 금액은" + 5 + 10 + "입니다.");
+console.log("총 금액은" + (5 + 10) + "입니다.");
+console.log(`총 금액은 ${5 + 10}입니다.`);
+
+let homework = true;
+console.log(`아 숙제 ${homework ? "다했다." : "아직 다 못했어."}`) 
