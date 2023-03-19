8bf4351c4dca6b1981a0d50593d7cc8f45568797
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-19 10:58:32 +0000

    #rotate Array

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const numArray = [1,2,3,4];
+//const numArray = [1,2,3,4];
 //배열의 길이 구하기
 //console.log(numArray.length);
 
@@ -11,8 +11,8 @@ const numArray = [1,2,3,4];
 //console.log(arr); //[ <2 empty items>, 3 ]
 
 //배열을 직접적으로 건드리는 방식
-numArray.push(5,6);
-console.log(numArray);
+//numArray.push(5,6);
+//console.log(numArray);
 
 //배열을 직접 건드리지 않는 방식
 //const result = numArray.concat(7,8);
@@ -20,5 +20,34 @@ console.log(numArray);
 //console.log(result);
 
 //배열을 직접적으로 건드리며 제거하는 메소드
-numArray.pop();
-console.log(numArray);
+//numArray.pop();
+//console.log(numArray);
+
+//배열 순회하기
+//기존의 방법
+//const nameArr = ['민철','영수','영희','민수'];
+//
+//for(var i=0; i<nameArr.length;i++){
+//    console.log(`내 이름은 ${nameArr[i]}야`);
+//}
+
+//forEach
+//nameArr.forEach(function(name){
+//    console.log(`내 이름은 ${name}야`);
+//})
+
+//nameArr.forEach(name=>console.log(`내 이름은 ${name}야`));
+
+//numArray.forEach(function(num){
+//    console.log(num);
+//})
+
+//numArray.forEach(function(){
+//    console.log(numArray);
+//})
+
+const oddArr = [1,3,5,7,9];
+
+//oddArr.map(num=>{console.log(num*2)});
+const newArr = oddArr.map(num=>num*2);
+console.log(newArr);
