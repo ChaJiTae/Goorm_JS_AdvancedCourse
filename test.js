ee3419e3c6630843e03cba3898d288db6052829b
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-19 10:00:33 +0000

    #array add,remove

--- test.js
+++ test.js
@@ -1,8 +1,24 @@
 const numArray = [1,2,3,4];
-console.log(numArray.length);
+//배열의 길이 구하기
+//console.log(numArray.length);
 
-const arr = [];
-arr[2] = 3;
+//배열의 값 지정하기
+//const arr = [];
+//arr[2] = 3;
 
-console.log(arr.length); // 3
-console.log(arr); //[ <2 empty items>, 3 ]
+//배열의 길이와 배열의 요소 갯수는 같은가? -> NO
+//console.log(arr.length); // 3
+//console.log(arr); //[ <2 empty items>, 3 ]
+
+//배열을 직접적으로 건드리는 방식
+numArray.push(5,6);
+console.log(numArray);
+
+//배열을 직접 건드리지 않는 방식
+//const result = numArray.concat(7,8);
+//console.log(numArray);
+//console.log(result);
+
+//배열을 직접적으로 건드리며 제거하는 메소드
+numArray.pop();
+console.log(numArray);
