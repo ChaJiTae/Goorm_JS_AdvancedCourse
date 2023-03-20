2a3ad0137d9ee5febd3595ffc1f9a8a616b897a1
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-20 14:02:25 +0000

    #object spread

--- test.js
+++ test.js
@@ -1,91 +1,19 @@
-const song = {author : '비',date: '2020-05-31',title:'깡',body:'화려한 조명이 나를 감싸네이예에'};
+const student = {
+    name : '김구름',
+    number : '17',
+};
 
-//키 값 이름으로 변수를 선언하는 방법 - 구조분해할당
-//const {author,date,title,body} = song;
-//console.log(author,date,title,body);
-
-//키 값과 다른 이름으로 변수를 선언하는 방법 - 구조분해할당
-//const {author:name,date:time,title:mainTitle,body:lyric} = song;
-//console.log(name,time,mainTitle,lyric);
-
-//예시)
-//HTTP/1.1 200 OK
-//Content-type: application/json;charset=UTF-8
-var kakaoResponse = {
-    "tid": "T1234567890123456789",
-    "cid": "TC0ONETIME",
-    "status": "SUCCESS_PAYMENT",
-    "partner_order_id": "partner_order_id",
-    "partner_user_id": "partner_user_id",
-    "payment_method_type": "MONEY",
-    "item_name": "초코파이",
-    "quantity": 1,
-    "amount": {
-        "total": 2200,
-        "tax_free": 0,
-        "vat": 200,
-        "point": 0,
-        "discount": 0,
-        "green_deposit": 0
-    },
-    "canceled_amount": {
-        "total": 0,
-        "tax_free": 0,
-        "vat": 0,
-        "point": 0,
-        "discount": 0,
-        "green_deposit": 0
-    },
-    "cancel_available_amount": {
-        "total": 2200,
-        "tax_free": 0,
-        "vat": 200,
-        "point": 0,
-        "discount": 0,
-        "green_deposit": 0
-    },
-    "created_at": "2016-11-15T21:18:22",
-    "approved_at": "2016-11-15T21:20:48",
-    "payment_action_details": [
-        {
-            "aid": "A5678901234567890123",
-            "payment_action_type": "PAYMENT",
-            "payment_method_type": "MONEY",
-            "amount": 2200,
-            "point_amount": 0,
-            "discount_amount": 0,
-            "approved_at": "2016-11-15T21:20:48",
-            "green_deposit": 0
-        }
-    ]
+const univStudent = {
+    ...student,
+    major : '컴퓨터공학과',
 }
 
-//const {item_name , amount:{vat} , payment_action_details : [{aid}],payment_action_details:[{amount:totalPrice}]}=kakaoResponse;
-//console.log(item_name,vat,aid,totalPrice);
+console.log(student);
+console.log(univStudent);
 
-//const {item_name : name,approved_at : date,amount:{total : total_price}}=kakaoResponse;
-//console.log(name,date,total_price);
+const koreanUnivStudent = {
+    ...univStudent,
+    region : 'seoul',
+}
 
-//const arr = [1,2,3];
-//const [one,two,three] = arr;
-//console.log(one,two,three); // 1 2 3
-
-//let [x,y,z] = [1,2,3];
-//console.log(x); // 1
-//console.log(y); // 2
-//console.log(z); // 3
-
-//let [a,b,c = 3] = [1,2];
-//console.log(x,y,z); // 1 2 3
-
-const nameArr = ['영희','철수','가온'];
-const [name1,name2,name3] = nameArr;
-console.log(name1,name2,name3);
-
-let [x,y,z] = [12,23,34];
-console.log(x);
-console.log(y);
-console.log(z);
-
-let [a,b,c=2] = [1,3];
-console.log(a,b,c);
+console.log(koreanUnivStudent);
