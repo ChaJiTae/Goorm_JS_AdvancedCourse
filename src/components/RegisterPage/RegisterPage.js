2075d659ae6f516c400a00bc830a6f9271764f40
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-27 14:20:56 +0000

    #react-hook-form

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useForm} from 'react-hook-form'
 
 function RegisterPage() {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({mode:"onChange"});
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("email"));
+
   return (
     <div className='auth-wrapper'>
         <div style={{textAlign : 'center'}}>
           <h3>Register</h3>
         </div>  
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           
           <label>Email</label>
           <input
               name="email" 
               type="email"  
-          />
+              {...register("email", { required: true,maxLength:10 })}/>
+              {errors.email && <span>This field is required</span>}
           
           <label>Name</label>
           <input
