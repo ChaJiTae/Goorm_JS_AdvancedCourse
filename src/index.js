ff8519f01d313648fd839990d80b92b7229cf08a
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-03-27 04:09:11 +0000

    #redux setting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './redux/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStoreWithMiddleware(Reducer,
+				window.__REDUX_DEVTOOLS_EXTENTION__&&
+				window.__REDUX_DEVTOOLS_EXTENTION__()
+		)}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
