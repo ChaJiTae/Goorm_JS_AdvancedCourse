6b9b25fb7426ff60ef95768dbe263912a94cf18b
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-04-01 14:49:12 +0000

    #react-router-dom version update로 인한 useHistory 사용불가로 app.js와 index.js 코드 전면 수정

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
-import './App.css';
-import React,{useEffect} from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-import ChatPage from './components/ChatPage/ChatPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
+import ChatPage from "./components/ChatPage/ChatPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
-//firebase import 부분
+// firebase import 부분
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 function App(props) {
+const navigate = useNavigate();
 
-  useEffect(()=>{
-    firebase.auth().onAuthStateChanged(user=>{
-      console.log('user', user)
-      //로그인이 된 상태
-      if(user){
-          props.history.push("/");
-      }
-      //로그인이 되지 않은 상태
-      else{
-          props.history.push("/login");
-      }
-    })
-  },[])
+useEffect(() => {
+firebase.auth().onAuthStateChanged((user) => {
+console.log("user", user);
+// 로그인이 된 상태
+if (user) {
+navigate("/");
+}
+// 로그인이 되지 않은 상태
+else {
+navigate("/login");
+}
+});
+}, [navigate]);
 
-  return (
-    <Router>
-        <Routes>
-          <Route exact path="/" element = {<ChatPage/>} />
-          <Route path="/login" element = {<LoginPage />} />
-          <Route path="/register" element = {<RegisterPage />} />
-        </Routes>
-    </Router>
-  );
+return (
+<Routes>
+<Route exact path="/" element={<ChatPage />} />
+<Route path="/login" element={<LoginPage />} />
+<Route path="/register" element={<RegisterPage />} />
+</Routes>
+);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore,applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './redux/reducers';
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={createStoreWithMiddleware(Reducer,
-				window.__REDUX_DEVTOOLS_EXTENTION__&&
-				window.__REDUX_DEVTOOLS_EXTENTION__()
-		)}>
-      <App />
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
+      <Router>
+        <App />
+      </Router>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
