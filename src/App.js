2f21206ee296075511dd9fefb3a822e1d4fd385d
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-04-02 08:48:39 +0000

    #Logout Status but Enter ChatPage Issue debuging

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import ChatPage from "./components/ChatPage/ChatPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
+import UserPage from "./components/UserPage/UserPage";
 
 // firebase import 부분
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import { Navigate } from "react-router-dom";
 
 function App(props) {
   const navigate = useNavigate();
@@ -23,16 +25,18 @@ function App(props) {
       }
       // 로그인이 되지 않은 상태
       else {
-        navigate("/login");
+        //register 페이지나 login 페이지로 이동되게끔 작성됨
+        //만약 ChatPage 말고 다른 페이지가 있을 경우 제어해야함
       }
     });
   }, [navigate]);
 
   return (
     <Routes>
-      <Route exact path="/" element={<ChatPage />} />
+      <Route path="/" element={firebase.auth().currentUser ? (<ChatPage />) : (<Navigate to="/login" replace={true} />)}/>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/user" element={firebase.auth().currentUser ? (<UserPage />) : (<Navigate to="/login" replace={true} />)}/>
     </Routes>
   );
 }
