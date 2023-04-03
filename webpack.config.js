12155904addddc9db641c06135fe397873add2fb
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-04-03 14:23:50 +0000

    #오류 해결 불가

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      fs: false,
+      child_process: false,
+      crypto: false,
+      os: false,
+    }
+  },
+  node: {
+    process: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
