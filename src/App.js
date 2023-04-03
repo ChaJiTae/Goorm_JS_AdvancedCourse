e5dbe8adba55f48c811c6f6a7d135acf11cadaa2
지태 <55056889+Ideal-JiTae@users.noreply.github.com>
2023-04-03 09:53:51 +0000

    #Login Logout and complete about Singin Page issue!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 
 import ChatPage from "./components/ChatPage/ChatPage";
 import LoginPage from "./components/LoginPage/LoginPage";
@@ -12,46 +12,63 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 import { Navigate } from "react-router-dom";
 
-import { useDispatch,useSelector } from "react-redux";
-import { setUser , clearUser}from './redux/actions/user_action';
+import { useDispatch, useSelector } from "react-redux";
+import { setUser, clearUser } from "./redux/actions/user_action";
 
 function App(props) {
   const navigate = useNavigate();
-  let dispatch = useDispatch();
-  const isLoading = useSelector(state=>state.user.isLoading);
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.user.isLoading);
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       console.log("user", user);
       // 로그인이 된 상태
       if (user) {
         navigate("/");
-        //dispatch(setUser(user));
+        dispatch(setUser(user));
       }
       // 로그인이 되지 않은 상태
       else {
-        //register 페이지나 login 페이지로 이동되게끔 작성됨
-        //만약 ChatPage 말고 다른 페이지가 있을 경우 제어해야함
-        // history.push("/login")
-        //dispatch(clearUser())
+        // 로그인되지 않은 경우
+        // 현재 경로를 받아옵니다.
+        const currentPath = location.pathname;
+  
+        // 로그인 페이지로 이동합니다.
+        if (
+          currentPath !== "/login" &&
+          currentPath !== "/register" // 이 부분을 추가하여 회원가입 페이지로 이동할 수 있도록 합니다.
+        ) {
+          navigate("/login", { state: { from: currentPath }, replace: true });
+        }
+  
+        // 사용자 정보를 초기화합니다.
+        dispatch(clearUser());
       }
     });
-  }, [navigate]);
+  }, [navigate, dispatch, location]);
 
-  // if(isLoading){
-  //   return (
-  //     <div>
-  //       ...Loading
-  //     </div>
-  //   )
-  // } else{
+  if (isLoading) {
+    return <div>...Loading</div>;
+  } else {
     return (
       <Routes>
-        <Route path="/" element={firebase.auth().currentUser ? (<ChatPage />) : (<Navigate to="/login" replace={true} />)}/>
+        <Route
+          path="/"
+          element={
+            firebase.auth().currentUser ? (
+              <ChatPage />
+            ) : (
+              <Navigate to="/login" replace={true} />
+            )
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     );
   }
-//}
+}
 
 export default App;
--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -2,15 +2,16 @@ import React from "react";
 import { IoIosChatboxes } from "react-icons/io";
 import Dropdown from "react-bootstrap/Dropdown";
 import Image from 'react-bootstrap/Image';
-// import { useSelector } from "react-redux";
-//import firebase from '../../../firebase';
+import { useSelector } from "react-redux";
+import { auth } from '../../../firebase';
 
 function UserPanel() {
-  // const user = useSelector(state=>state.user.currentUser);
+  const user = useSelector(state=>state.user.currentUser);
 
-  //const handleLogout = () => {
-  //  firebase.auth().signOut();
-  //}
+  const handleLogout = () => {
+   auth.signOut();
+  }
+
   return (
     <div>
       {/*Logo*/}
@@ -19,19 +20,17 @@ function UserPanel() {
       </h3>
 
       <div style={{ display: "flex", marginBottom: "1rem" }}>
-        <Image src="" style={{width:'30px',height:'30px',marginTop:'3px'}} roundedCircle/>
-        {/* <Image src={user&&user.photoURL} style={{width:'30px',height:'30px',marginTop:'3px'}} roundedCircle/> */}
+        {user && <Image src={user.photoURL} style={{width:'30px',height:'30px',marginTop:'3px'}} roundedCircle/>}
         <Dropdown>
           <Dropdown.Toggle 
             style={{background:'transparent',border:'0px'}}
           id="dropdown-basic">
-            {/* {user.displayName} */}
+            {user && user.displayName}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
             <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
-            {/* <Dropdown.Item href="#/action-2" onClick={handleLogout}>로그아웃</Dropdown.Item> */}
-            <Dropdown.Item href="#/action-2">로그아웃</Dropdown.Item>
+            <Dropdown.Item href="#/action-2" onClick={handleLogout}>로그아웃</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </div>
